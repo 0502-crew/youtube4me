d218b8a934a02305476b51586ed0e17f91f281bf
Martijn <mvanloocke@gmail.com>
2020-04-03 18:29:36 +0200

    Add refresh button to navbar

--- client/src/components/navbar/NavBar.tsx
+++ client/src/components/navbar/NavBar.tsx
@@ -1,11 +1,15 @@
 import './NavBar.css';
 import * as React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSyncAlt } from '@fortawesome/free-solid-svg-icons';
 
 export class NavBar extends React.Component {
   render(): React.ReactNode {
     return (
       <div className='navbar'>
         <a href="#">Notifications</a>
+        <span className="fill"></span>
+        <span className="refresh" onClick={() => document.location.reload()}><FontAwesomeIcon icon={faSyncAlt} /></span>
       </div>
     );
   }
